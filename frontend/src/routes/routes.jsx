0b93b44d92578040167bde81ee618f86a947b667
Abhishek <satheabhishek1@gmail.com>
2024-10-13 18:46:37 +0530

    started frontend dude

---
+++ frontend/src/routes/routes.jsx
@@ -0,0 +1,13 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Login from "../pages/Login";
+import App from "../App";
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />
+  },
+  {
+    path: '/login',
+    element: <Login />
+  }
+]);
